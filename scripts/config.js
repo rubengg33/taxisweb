5bfd9cbb40d1fcbf776364f70bb5bdf2e85f40bb
rubengg33 <rubentrollyt@gmail.com>
2025-03-24 13:41:44 +0100

    cambios config

---
+++ scripts/config.js
@@ -0,0 +1,9 @@
+let API_URL = "";
+
+fetch("/config")
+  .then(res => res.json())
+  .then(config => {
+    API_URL = config.apiUrl;
+    console.log("API URL cargada:", API_URL);
+  })
+  .catch(err => console.error("Error cargando API_URL:", err));
