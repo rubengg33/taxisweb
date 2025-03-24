0096e836c8ca28e98045bd9abbfd485ce714b44f
rubengg33 <rubentrollyt@gmail.com>
2025-03-24 14:50:12 +0100

    probando

--- scripts/config.js
+++ scripts/config.js
@@ -1,11 +1,11 @@
-// Si tienes la URL del backend como una variable de entorno, puedes usarla
+// Define la URL base
 let API_URL = "https://taxisweb.onrender.com";  // URL completa del backend
 
 fetch(`${API_URL}/api/config`)
   .then(res => res.json())
   .then(config => {
-    API_URL = config.apiUrl;
-    console.log("API URL cargada:", API_URL);
+    const backendAPIUrl = config.apiUrl; // Usamos una variable diferente para almacenar la URL
+    console.log("API URL cargada:", backendAPIUrl);
+    // Aquí puedes hacer lo que necesites con backendAPIUrl
   })
   .catch(err => console.error("Error cargando API_URL:", err));
-
