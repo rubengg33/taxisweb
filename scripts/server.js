d89606e6523f7b2d2829db51a3f42b2f2008f026
rubengg33 <rubentrollyt@gmail.com>
2025-05-29 14:51:27 +0200

    prueba

--- scripts/server.js
+++ scripts/server.js
@@ -94,7 +94,7 @@ app.post('/api/import', authenticateToken, validateApiKey, upload.single('file')
             }
           }
           results.push({
-            licencia: data['LICENCIA'] || '',
+            licencia: data['LICENCIA'] ? data['LICENCIA'].padStart(5, '0') : '',
             nombre_apellidos: data['CONDUCTOR'] || '',
             dni: data['DNI'] || '',
             email: data['CORREO ELECTRÉNICO'] || '',
