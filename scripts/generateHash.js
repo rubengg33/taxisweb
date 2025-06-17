1edabb2d806e93037090892210febef7eaab9bbd
rubengg33 <135147356+rubengg33@users.noreply.github.com>
2025-06-17 22:51:07 +0200

    Cambio comentario

--- scripts/generateHash.js
+++ scripts/generateHash.js
@@ -1,7 +1,7 @@
 const bcrypt = require('bcryptjs');
 
 async function generateHash() {
-    const password = "Wetaxiadmins2025"; // Your desired initial password
+    const password = "Wetaxiadmins2025"; // Contraseña que quieras
     const hash = await bcrypt.hash(password, 10);
     console.log('Hashed password:', hash);
 }
